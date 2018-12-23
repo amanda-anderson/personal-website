4b6eb2998c3d1420bc4a1e42d896604b2e37083a
Amanda <anderson_amanda@mail.com>
2018-12-22 21:05:51 -0500

    Added js

--- js/app.js
+++ js/app.js
@@ -10,4 +10,28 @@ $("a").click(function (e) {
     $('html, body').animate({
         scrollTop: $("#" + btnId).offset().top
     }, 1000);
+
+    if (e.currentTarget.id == 3) {
+        jQuery('#latestJob').focus();
+    }
 });
+
+// Function to display job description on button click
+function displayJobDesc(btnID, title, date, company) {
+    document.getElementById("selected-job-title").innerHTML = title;
+    document.getElementById("date-worked").innerHTML = date;
+    document.getElementById("company-name").innerHTML = company;
+
+    if (btnID == 1) {
+        document.getElementById("job-desc-points").innerHTML = '<li>Job Point 1</li>';
+    }
+
+    if (btnID == 2) {
+        document.getElementById("job-desc-points").innerHTML = '<li>Job Point 2</li>';
+    }
+  }
+
+  // Set latest job as default display in Experience
+jQuery(function(){
+    jQuery('#latestJob').click();
+ });
