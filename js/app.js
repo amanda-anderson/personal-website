bb5348f5829755f48f5420094e71dffc7c220a74
Amanda <anderson_amanda@mail.com>
2018-12-23 11:41:28 -0500

    Modals

--- js/app.js
+++ js/app.js
@@ -78,3 +78,25 @@ function displayJobDesc(btnID, title, date, company) {
 jQuery(function(){
     jQuery('#latestJob').click();
  });
+
+ // Modal popup for portfolio
+var modal = document.getElementById('myModal');
+var btn = document.getElementById('news-collective');
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
