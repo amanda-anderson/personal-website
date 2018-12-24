f0868d6c9a6dad8402b6f08cd1dc6cd4b6a42efb
Amanda <anderson_amanda@mail.com>
2018-12-23 19:36:39 -0500

    php added

---
+++ email-form.php
@@ -0,0 +1,14 @@
+<?php
+   $first_name = $_POST['first_name'];
+   $last_name = $_POST['last_name'];
+   $telephone = $_POST['telephone'];
+   $email = $_POST['email'];
+   $header = $_POST['email'];
+   $message = $_POST['message'];
+   $message .= "\n\n" . $telephone;
+   $recipient = "anderson.amanda403@gmail.com";
+   $mailheader = "From: $email \r\n";
+   mail($recipient, $header, $message, $telephone) or die("Error!");
+   echo "<script>console.log('name: " . $first_name. "');</script>";
+   echo "Thank You!";
+?>
